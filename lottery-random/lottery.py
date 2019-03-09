650948d91d65efbe690aaca6b7967a2472d2709d
mkierc <urban.pl@gmail.com>
2019-03-09 14:20:57 +0100

    add lottery random numbers generator, update gitignore and requirements

---
+++ lottery-random/lottery.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+from rdoclient_py3 import RandomOrgClient
+
+
+def main():
+    api_key = open(os.path.join(sys.path[0], 'api.key')).readline()
+    rand = RandomOrgClient(api_key)
+    print(f'{rand.get_requests_left()} requests from random.org are left available today.')
+
+    first_five = rand.generate_integers(n=5, min=1, max=50, replacement=False)
+    first_two = rand.generate_integers(n=2, min=1, max=10, replacement=False)
+    second_five = rand.generate_integers(n=5, min=1, max=50, replacement=False)
+    second_two = rand.generate_integers(n=2, min=1, max=10, replacement=False)
+
+    print(f'{first_five} {first_two}\n{second_five} {second_two}')
+
+
+if __name__ == "__main__":
+    main()
