af20f9f7ec15516cd92232fae813179b68ba4df0
mkierc <urban.pl@gmail.com>
2021-04-26 19:59:30 +0200

    Updated lottery.py to also generate Lotto numbers

--- lottery-random/lottery.py
+++ lottery-random/lottery.py
@@ -9,12 +9,19 @@ def main():
     rand = RandomOrgClient(api_key)
     print(f'{rand.get_requests_left()} requests from random.org are left available today.')
 
-    first_five = sorted(rand.generate_integers(n=5, min=1, max=50, replacement=False))
-    first_two = sorted(rand.generate_integers(n=2, min=1, max=10, replacement=False))
-    second_five = sorted(rand.generate_integers(n=5, min=1, max=50, replacement=False))
-    second_two = sorted(rand.generate_integers(n=2, min=1, max=10, replacement=False))
+    if len(sys.argv) > 1 and sys.argv[1].lower() == 'lotto':
+        first_six = sorted(rand.generate_integers(n=6, min=1, max=49, replacement=False))
+        second_six = sorted(rand.generate_integers(n=6, min=1, max=49, replacement=False))
 
-    print(f'{first_five} {first_two}\n{second_five} {second_two}')
+        print(f'{first_six}\n{second_six}')
+
+    else:
+        first_five = sorted(rand.generate_integers(n=5, min=1, max=50, replacement=False))
+        first_two = sorted(rand.generate_integers(n=2, min=1, max=10, replacement=False))
+        second_five = sorted(rand.generate_integers(n=5, min=1, max=50, replacement=False))
+        second_two = sorted(rand.generate_integers(n=2, min=1, max=10, replacement=False))
+
+        print(f'{first_five} {first_two}\n{second_five} {second_two}')
 
 
 if __name__ == "__main__":
